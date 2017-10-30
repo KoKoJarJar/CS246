c48457f991ceffb938b93a201497c2116ada8fc8
JarJar7 <ardavan.behnia@gmail.com>
2017-10-30 02:39:34 -0500

    default

---
+++ vowel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+usage () {
+  echo "This command needs a valid file name"
+}
+if [ $# -lt 1 ]; then
+  usage
+  exit 1;
+elif [ ! \( -f ${1} \) ]; then
+  usage
+  exit 1
+fi
+file_name="${1}"
+egrep -i "a" "${file_name}" | \
+egrep -i "e" | egrep -i "u" | egrep -i "o" | \
+egrep -i "i"
