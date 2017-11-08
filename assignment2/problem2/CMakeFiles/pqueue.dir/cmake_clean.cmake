53f4a757601cfaf5247dc0302153b13c694e5ec4
JarJar7 <ardavan.behnia@gmail.com>
2017-11-08 12:19:41 -0600

    test 3

---
+++ assignment2/problem2/CMakeFiles/pqueue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pqueue.dir/nostaticexits.cpp.o"
+  "pqueue.pdb"
+  "pqueue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pqueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
