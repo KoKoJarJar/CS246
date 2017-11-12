6c88f04013109a51094e8b543151d60e4dded7e3
JarJar7 <ardavan.behnia@gmail.com>
2017-11-11 19:35:40 -0600

    default

--- assignment2/problem3/ppm.cpp
+++ assignment2/problem3/ppm.cpp
@@ -10,14 +10,14 @@ struct PpmImage {
   struct Pixel {
     unsigned char r, g, b;
   };
-  unsigned int len, height, colourmax;
+  unsigned int width, height, colourmax;
   Pixel **pixels;
 };
 
 int main(int argc, char const *argv[]) {
-  short tranpose = 0;
+  short tran = 0;
   short vertical = 0;
-//  short grey_scale = 0;
+  //  short grey_scale = 0;
   for (int i = 1; i < argc; ++i) {
     try {
       const string option(argv[i]);
@@ -25,10 +25,10 @@ int main(int argc, char const *argv[]) {
         ++vertical;
         vertical %= 2;
       } else if (option == "-t") {
-        ++tranpose;
-        tranpose %= 2;
+        ++tran;
+        tran %= 2;
       } else if (option == "-g") {
-//        grey_scale = 1;
+        //        grey_scale = 1;
       } else {
         throw "not a valid argument";
       }
@@ -39,39 +39,142 @@ int main(int argc, char const *argv[]) {
       std::cerr << "Error handling command arguments" << std::endl;
     }
   }
-  const PpmImage * const new_file=read_ppm();
-  std::cout << new_file->len << std::endl;
+  PpmImage *image = read_ppm();
+  grey_scale(image);
+  write_ppm(image);
   return 0;
 }
 
 // Look at header file for description
-const PpmImage *read_ppm() {
+PpmImage *read_ppm() {
   std::cout.exceptions(std::ofstream::failbit | std::ofstream::badbit);
   std::cerr.exceptions(std::ofstream::failbit | std::ofstream::badbit);
   std::cin.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  bool completed = false;
+  PpmImage *return_file = nullptr;
   try {
-    auto *return_file = new PpmImage;
-    string garbage;
-    std::cin >> garbage;
-    std::cin >> return_file->len;
+    return_file = allocate_image();
+    string name;
+    std::cin >> name;
+    if (name != "P6") {
+      throw "error";
+    }
+    std::cin >> return_file->width;
     std::cin >> return_file->height;
-    if (return_file->len <= 0 || return_file->height <= 0) {
-      throw "incorrect length or height";
+    if (static_cast<int>(return_file->width) <= 1 ||
+        static_cast<int>(return_file->height) <= 1) {
+      throw "error";
     }
     std::cin >> return_file->colourmax;
+    std::cin.ignore(1);
     return_file->pixels = new PpmImage::Pixel *[return_file->height];
     for (unsigned int i = 0; i < return_file->height; ++i) {
-      return_file->pixels[i] = new PpmImage::Pixel[return_file->len];
+      return_file->pixels[i] = new PpmImage::Pixel[return_file->width];
     }
     for (unsigned int i = 0; i < return_file->height; ++i) {
-     for (unsigned int j = 0; j < return_file->len; ++j) {
-       std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].r)), sizeof(char));
-       std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].g)), sizeof(char));
-       std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].b)), sizeof(char));
-     }
+      for (unsigned int j = 0; j < return_file->width; ++j) {
+        std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].r)),
+                      sizeof(unsigned char));
+        std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].g)),
+                      sizeof(unsigned char));
+        std::cin.read(reinterpret_cast<char *>(&(return_file->pixels[i][j].b)),
+                      sizeof(unsigned char));
+      }
     }
+    completed = true;
+    char useless = 0;
+    std::cin.read(&useless, sizeof(char));
+    throw "error";
+  } catch (std::bad_alloc &err) {
+    std::cerr << "allocation error, exiting" << std::endl;
+    exit(EXIT_FAILURE);
+  } catch (...) {
+    if (completed) {
+      return return_file;
+    }
+    // cleanup
+    throw "I/O error, format of the file does not match a P6 PPM";
+  }
+}
+
+// Look at header file for description
+PpmImage *allocate_image() {
+  PpmImage *return_file = nullptr;
+  try {
+    return_file = new PpmImage;
+    return_file->width = 0;
+    return_file->height = 0;
+    return_file->colourmax = 0;
+    return_file->pixels = nullptr;
     return return_file;
   } catch (...) {
+    std::cerr << "allocation error, exiting" << std::endl;
     exit(EXIT_FAILURE);
+  }
+}
+
+// Look at header file for description
+void vertical_flip(PpmImage *image) {
+  for (unsigned int i = 0; i < image->height / 2; ++i) {
+    PpmImage::Pixel *temp = image->pixels[i];
+    image->pixels[i] = image->pixels[image->height - 1 - i];
+    image->pixels[image->height - 1 - i] = temp;
+  }
+}
+
+// Look at header file for description
+void transpose(PpmImage *image) {
+  try {
+    unsigned int temp = image->height;
+    image->height = image->width;
+    image->width = temp;
+    PpmImage::Pixel **temp_pixels = image->pixels;
+    image->pixels = new PpmImage::Pixel *[image->height];
+    for (unsigned int i = 0; i < image->height; ++i) {
+      image->pixels[i] = new PpmImage::Pixel[image->width];
+    }
+    for (unsigned int i = 0; i < image->height; ++i) {
+      for (unsigned int j = 0; j < image->width; ++j) {
+        image->pixels[i][j] = temp_pixels[j][i];
+      }
+    }
+    for (unsigned int i = 0; i < image->width; ++i) {
+      delete[] temp_pixels[i];
+    }
+    delete[] temp_pixels;
+  } catch (...) {
+    std::cerr << "allocation error, exiting" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+// Look at header file for description
+void grey_scale(PpmImage *image) {
+  for (unsigned int i = 0; i < image->height; ++i) {
+    for (unsigned int j = 0; j < image->width; ++j) {
+      unsigned char average = image->pixels[i][j].r / 3 +
+                              image->pixels[i][j].g / 3 +
+                              image->pixels[i][j].b / 3;
+      image->pixels[i][j].b = average;
+      image->pixels[i][j].g = average;
+      image->pixels[i][j].r = average;
+    }
+  }
+}
+
+// Look at header file for description
+void write_ppm(PpmImage *image) {
+  std::cout << "P6 ";
+  std::cout << image->width << " " << image->height;
+  std::cout << " " << image->colourmax << std::endl;
+  for (unsigned int i = 0; i < image->height; ++i) {
+    for (unsigned int j = 0; j < image->width; ++j) {
+      std::cout.write(reinterpret_cast<char *>(&(image->pixels[i][j].r)),
+                      sizeof(unsigned char));
+      std::cout.write(reinterpret_cast<char *>(&(image->pixels[i][j].g)),
+                      sizeof(unsigned char));
+      std::cout.write(reinterpret_cast<char *>(&(image->pixels[i][j].b)),
+                      sizeof(unsigned char));
+    }
   }
 }
--- assignment2/problem3/ppm.h
+++ assignment2/problem3/ppm.h
@@ -12,6 +12,41 @@ struct PpmImage;
 // complexity: I/O + O(mn) where m is the number of columns and
 // n is the number of rows
 // restrictions: only handles a P6 PPM
-// exception_handling: throws exception when input does not
-// match the format of a p6 PPM
-const PpmImage *read_ppm();
+// exception_handling: throws exception of type const char * and cleans up
+// in case of allocation error, exits the program
+PpmImage *read_ppm();
+
+// allocate_image() allocates and initializes a PpmImage 
+// structure
+// effects: image is created and allocated (caller must free)
+// exception_handling: exits the program in case of bad_alloc
+PpmImage *allocate_image();
+
+// vertical_flip(image) transforms the image by doing a vertical flip
+// effects: modifies image
+// complexity: O(n) where n is the height of the image 
+// restrictions: only handles a P6 PPM
+// exception_handling: safe
+void vertical_flip(PpmImage *image);
+
+// transpose(image) transforms the image by doing a transpose
+// effects: modifies image
+// complexity: O(mn) where m and n are the width and height of the image
+// restrictions: only handles a P6 PPM
+// exception_handling: safe
+void transpose(PpmImage *image);
+
+
+// grey_scale(image) transforms the image by doing a grey_scale transformation
+// effects: modifies image
+// complexity: O(mn) where m and n are the width and height of the image
+// restrictions: only handles a P6 PPM
+// exception_handling: safe
+void grey_scale(PpmImage *image);
+
+// write_ppm(image) writes the ppm file to stdout
+// complexity: I/O + O(mn) as read_ppm
+// restrictions: only handles a P6 PPm
+// exception_handling: safe
+void write_ppm(PpmImage *image);
+
