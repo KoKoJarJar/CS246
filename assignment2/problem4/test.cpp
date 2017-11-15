f5e49d8df4ad9dbf3d21c5c26fa1b174017fdd52
JarJar7 <ardavan.behnia@gmail.com>
2017-11-15 00:44:40 -0600

    default

--- assignment2/problem4/test.cpp
+++ assignment2/problem4/test.cpp
@@ -6,7 +6,32 @@
 #define _ASSERT_
 #include <cassert>
 #endif /* end of include guard */
+#ifndef _IOSTREAM_
+#define _IOSTREAM_
+#include <iostream>
+#endif /* end of include guard */
+#ifndef _STRING_
+#define _STRING_
+#include <string>
+#endif /* end of include guard */
 
 int main() {
-  return 0;
+  Stack *stack = allocate_stack();
+  add_stack("boz", stack);
+  add_stack("lame", stack);
+  add_stack("meh", stack);
+  assert(show_stack(stack) == "meh");
+  assert(show_stack(stack) == "meh");
+  assert(search_stack("meh", stack));
+  assert(search_stack("lame", stack));
+  assert(search_stack("boz", stack));
+  assert(!search_stack("lul", stack));
+  assert(!search_stack("lel", stack));
+  pop_stack(stack);
+  assert(show_stack(stack) == "lame");
+  pop_stack(stack);
+  assert(show_stack(stack) == "boz");
+  pop_stack(stack);
+  assert(show_stack(stack) == "empty");
+  destroy_stack(stack);
 }
