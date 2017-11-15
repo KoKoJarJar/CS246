0c340b956435b489c7fc57f5918bc584b9905c67
JarJar7 <ardavan.behnia@gmail.com>
2017-11-15 00:34:12 -0600

    added test

---
+++ assignment2/problem4/test.cpp
@@ -0,0 +1,12 @@
+#ifndef _INCLUDE_
+#define _INCLUDE_
+#include "include.h"
+#endif /* end of include guard */
+#ifndef _ASSERT_
+#define _ASSERT_
+#include <cassert>
+#endif /* end of include guard */
+
+int main() {
+  return 0;
+}
